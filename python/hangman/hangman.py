e7c68707289793c085075fe96d79c79877494cda
moeka-m <moeka.dev@gmail.com>
2021-03-21 09:12:26 +0900

    fix: pyhton hangman データの持ち方を改良

--- python/hangman/hangman.py
+++ python/hangman/hangman.py
@@ -11,42 +11,28 @@ class Hangman:
         self.remaining_guesses = 9
         self.status = STATUS_ONGOING
         self.word = word
-        self.mask = [True] * len(word)
+        self.guesses: list[str] = []
 
     def guess(self, char: str):
         if not self.status == STATUS_ONGOING:
             raise ValueError("The game already finished.")
 
-        is_correct_guess = False
-
-        for correct_char, masked, index in zip(
-            self.word, self.mask, range(len(self.word))
-        ):
-            if masked:
-                if correct_char == char:
-                    self.mask[index] = False
-                    is_correct_guess = True
-
-        if is_correct_guess:
-            if not any(self.mask):
-                self.status = STATUS_WIN
-        else:
+        if char in set(self.guesses) or char not in self.word:
             self.remaining_guesses -= 1
             if self._is_hanged():
                 self.status = STATUS_LOSE
+
+        self.guesses.append(char)
+        if all(c in self.guesses for c in self.word):
+            self.status = STATUS_WIN
 
     def _is_hanged(self) -> bool:
         return self.remaining_guesses < 0
 
     def get_masked_word(self) -> str:
-        masked_word: list[str] = []
-        for (char, masked) in zip(self.word, self.mask):
-            if masked:
-                masked_word.append(MASKED_CHAR_DISPLAY)
-            else:
-                masked_word.append(char)
-
-        return "".join(masked_word)
+        return "".join(
+            c if c in self.guesses else MASKED_CHAR_DISPLAY for c in self.word
+        )
 
     def get_status(self):
         return self.status
